a99253be58a683a8835bd19dde4b6835f3e5d5c0
Kareny <supermaps62@gmail.com>
2023-08-21 10:43:10 -0600

    Hola_mundo
    Actividad_1

---
+++ Tarea_/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tarea_',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
